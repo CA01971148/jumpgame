08919962295ea2021ed74d24babcc27fe4e4cf00
†村神† <107534447+CA01971172@users.noreply.github.com>
2022-09-23 11:44:32 +0900

    Implementation of class
    But,this program has a bug.It doesn't run.

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 "use strict";
-document.write('<img id="character" src="resource/rabbit.png">');
+document.write('<img id="character" src="resource/rabbit.png">'); //キャラ出現
+class character {
+    constructor() {
+        this.y = 0; //y座標
+    }
+    move() {
+        this.y += 32;
+        document.getElementById('character').style.top = this.y + "px";
+    }
+}
+let rabbit = new character();
+document.getElementById('character').onclick = rabbit.move();
--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-document.write( '<img id="character" src="resource/rabbit.png">' );
+document.write('<img id="character" src="resource/rabbit.png">')//キャラ出現
+class character{
+    private x:number//X座標
+    private y:number=0//y座標
+    private dx:number//x方向の速度
+    private dy:number//y方向の速度
+    move(){
+        this.y+=32;
+        document.getElementById('character').style.top=this.y+"px";
+    }
+}
+
+let rabbit=new character()
+
+document.getElementById('character').onclick=rabbit.move()
