1b20ee1fab0c27db14e689105378698ca6c9a659
†村神† <CA01971172@st.kawahara.ac.jp>
2022-09-26 09:21:21 +0900

    Try to correct the error
    Try to correct the error from type.
    But couldn't do.

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 document.write('<img id="character" src="resource/rabbit.png">')//キャラ出現
 class character{
-    private x:number//X座標
+    private x:number=0//X座標
     private y:number=0//y座標
-    private dx:number//x方向の速度
-    private dy:number//y方向の速度
+    private dx:number=0//x方向の速度
+    private dy:number=0//y方向の速度
     move(){
         this.y+=32;
-        document.getElementById('character').style.top=this.y+"px";
+        document.getElementById('character')!.style.top=this.y+"px";
     }
 }
 
 let rabbit=new character()
 
-document.getElementById('character').onclick=rabbit.move()
+document.getElementById('character')!.onclick=rabbit.move()
