da7e805042b1fa9a829956a86509a3beabf5012f
†村神† <CA01971172@st.kawahara.ac.jp>
2022-09-26 16:51:36 +0900

    Return to the runable state
    I don't find the correct way.
    Help me.

--- main.js
+++ main.js
@@ -3,20 +3,18 @@ document.write('<img id="character" src="resource/rabbit.png">'); //キャラ出
 class character {
     constructor() {
         this.x = 0; //X座標
-        this.y = 0; //y座標
+        this.y = 64; //y座標
         this.dx = 0; //x方向の速度
         this.dy = 0; //y方向の速度
     }
-    move(a) {
+    move() {
         rabbit.y += 32;
         document.getElementById('character').style.top = rabbit.y + "px";
     }
 }
 let rabbit = new character();
-/* document.getElementById('character')!.onclick=function(){
-    rabbit.y += 32
-    document.getElementById('character')!.style.top=rabbit.y+"px"
-} */
-let u;
-document.getElementById('character').onclick = u;
-rabbit.move(u);
+document.getElementById('character').onclick = function () {
+    rabbit.y += 32;
+    document.getElementById('character').style.top = rabbit.y + "px";
+};
+//document.getElementById('character')!.onclick=rabbit.move()
--- main.ts
+++ main.ts
@@ -1,23 +1,21 @@
 document.write('<img id="character" src="resource/rabbit.png">')//キャラ出現
 class character{
     private x:number=0//X座標
-    y:number=0//y座標
+    y:number=64//y座標
     private dx:number=0//x方向の速度
     private dy:number=0//y方向の速度
 
-/*     move(a:(b:GlobalEventHandlers)=>void){
+    public move(){
         rabbit.y += 32
         document.getElementById('character')!.style.top=rabbit.y+"px"
-    } */
+    }
 }
 
 let rabbit=new character()
 
-document.getElementById('character')!.onclick=function(){
+ document.getElementById('character')!.onclick=function(){
     rabbit.y += 32
     document.getElementById('character')!.style.top=rabbit.y+"px"
-}
+} 
 
-/* let u:any
-document.getElementById('character')!.onclick=u
-rabbit.move(u) */
+//document.getElementById('character')!.onclick=rabbit.move()
