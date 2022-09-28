0412bb0577bdf3b90f90c65a6d63fd17ade6b2d0
†村神† <CA01971172@st.kawahara.ac.jp>
2022-09-28 11:42:20 +0900

    Create the framework of movement
    Create the framework of movement responsive to key down
    But it doesn't run.The Character can't move.

--- main.js
+++ main.js
@@ -3,18 +3,97 @@ document.write('<img id="character" src="resource/rabbit.png">'); //キャラ出
 class character {
     constructor() {
         this.x = 0; //X座標
-        this.y = 64; //y座標
+        this.y = 0; //y座標
         this.dx = 0; //x方向の速度
         this.dy = 0; //y方向の速度
+        this.jumpVelocity = 0; //ジャンプ速度
+        this.moveVelocity = 10; //横移動科測量
+        this.secAdd = 0; //ジャンプ用時間計測変数
+        this.isOnGround = true; //接地しているかどうか
+        this.isSlip = false; //滑るかどうか
+        this.isCarry = false; //動かされているかどうか
+        this.isOnMoving = false; //動く床に乗っているかどうか
     }
     move() {
-        rabbit.y += 32;
-        document.getElementById('character').style.top = rabbit.y + "px";
+        this.x += this.dx;
+        this.y += this.dy;
+        if (this.isSlip === false) {
+            this.dx = 0;
+        }
+        document.getElementById('character').style.left = this.x + "px";
+        document.getElementById('character').style.left = this.y + "px";
+    }
+    moveLeft() {
+        this.dx -= this.moveVelocity;
+    }
+    moveRight() {
+        this.dx -= this.moveVelocity;
+        this.x += 20;
+        document.getElementById('character').style.left = this.x + "px";
+    }
+    jumpCharge() {
+    }
+    jump() {
+    }
+}
+class keyDown {
+    constructor() {
+        this.key_left = false; //左移動キーが押されているかどうか
+        this.key_right = false; //右移動キーが押されているかどうか
+        this.key_jump = false; //ジャンプキーが押されているかどうか
+    }
+    keyDownFunc(event) {
+        let key_code = event.keyCode;
+        switch (key_code) {
+            case 65: //「A」キーが押されたとき
+            case 37: //「←」キーが押されたとき
+                this.key_left = true;
+            case 68: //「D」キーが押されたとき
+            case 39: //「→」キーが押されたとき
+                this.key_right = true;
+            case 32: //「Space」キーが押されたとき
+            case 96: //「0(テンキー)」キーが押されたとき
+                this.key_jump = true;
+        }
+    }
+    keyUpFunc(event) {
+        let key_code = event.keyCode;
+        switch (key_code) {
+            case 65: //「A」キーが離されたとき
+            case 37: //「←」キーが離されたとき
+                this.key_left = false;
+            case 68: //「D」キーが離されたとき
+            case 39: //「→」キーが離されたとき
+                this.key_right = false;
+            case 32: //「Space」キーが離されたとき
+            case 96: //「0(テンキー)」キーが離されたとき
+                this.key_jump = false;
+        }
     }
 }
 let rabbit = new character();
-document.getElementById('character').onclick = function () {
-    rabbit.y += 32;
-    document.getElementById('character').style.top = rabbit.y + "px";
-};
-//document.getElementById('character')!.onclick=rabbit.move()
+let key = new keyDown();
+requestAnimationFrame(main); //メインループ、起動
+function main() {
+    addEventListener("keydown", key.keyDownFunc); //キーボードが押された時、keyDownFunc関数を呼び出す
+    addEventListener("keyup", key.keyUpFunc); //キーボードが離された時、keyUpFunc関数を呼び出す
+    /*     【仕様】
+        左右キーは同時に押すとどちらにも移動できない(どちらか片方を押しているときのみ移動できる)
+        ジャンプはジャンプキーを押している間に跳躍力を貯めて、ジャンプキーを離すと貯めた跳躍力の分だけ跳べる
+        跳躍力を貯めている間は接地中の移動ができない */
+    if (((key.key_left === true) && (key.key_right === false)) && (!((key.key_jump == true) && (rabbit.isOnGround == true)))) { //左移動キーが押されている間、moveLeft関数を呼び出す
+        rabbit.moveLeft();
+    }
+    if (((key.key_right === true) && (key.key_left === false)) && (!((key.key_jump == true) && (rabbit.isOnGround == true)))) { //右移動キーが押されている間、moveRight関数を呼び出す
+        rabbit.moveRight();
+    }
+    if (key.key_right === true) {
+        rabbit.x += 20;
+        document.getElementById('character').style.left = rabbit.x + "px";
+    }
+    if ((key.key_jump === true)) { //ジャンプキーが押されている間、jumpCharge関数を呼び出す
+        rabbit.jumpCharge();
+    }
+    rabbit.move();
+    requestAnimationFrame(main); ////main関数(自分自身)を呼び出すことでループさせる
+}
--- main.ts
+++ main.ts
@@ -1,21 +1,104 @@
 document.write('<img id="character" src="resource/rabbit.png">')//キャラ出現
 class character{
-    private x:number=0//X座標
-    y:number=64//y座標
-    private dx:number=0//x方向の速度
-    private dy:number=0//y方向の速度
+    x:number=0//X座標
+    y:number=0//y座標
+    dx:number=0//x方向の速度
+    dy:number=0//y方向の速度
+    readonly jumpVelocity:number=0//ジャンプ速度
+    readonly moveVelocity:number=10//横移動科測量
+    secAdd:number=0//ジャンプ用時間計測変数
+    isOnGround:boolean=true//接地しているかどうか
+    isSlip:boolean=false//滑るかどうか
+    isCarry:boolean=false//動かされているかどうか
+    isOnMoving:boolean=false//動く床に乗っているかどうか
 
-    public move(){
-        rabbit.y += 32
-        document.getElementById('character')!.style.top=rabbit.y+"px"
+    move(){//慣性で移動する関数
+        this.x+=this.dx
+        this.y+=this.dy
+        if(this.isSlip===false){
+            this.dx=0
+        }
+        document.getElementById('character')!.style.left =this.x+"px"
+        document.getElementById('character')!.style.left =this.y+"px"
+    }
+    moveLeft(){//左に移動する関数
+        this.dx-=this.moveVelocity
+    }
+    moveRight(){//右に移動する関数
+        this.dx-=this.moveVelocity
+        this.x+=20
+        document.getElementById('character')!.style.left =this.x+"px"
+    }
+    jumpCharge(){//跳躍力を貯める関数
+
+    }
+    jump(){//跳躍力を解放してジャンプする関数
+
+    }
+
+}
+
+class keyDown{//キーが押されているかどうか
+    key_left:boolean=false//左移動キーが押されているかどうか
+    key_right:boolean=false//右移動キーが押されているかどうか
+    key_jump:boolean=false//ジャンプキーが押されているかどうか
+
+    keyDownFunc(event:any){//キーボードが押されたときに呼び出される関数
+        let key_code = event.keyCode
+        switch(key_code){
+        case 65://「A」キーが押されたとき
+        case 37://「←」キーが押されたとき
+            this.key_left=true
+        case 68://「D」キーが押されたとき
+        case 39://「→」キーが押されたとき
+            this.key_right=true
+        case 32://「Space」キーが押されたとき
+        case 96://「0(テンキー)」キーが押されたとき
+            this.key_jump=true
+        }
+    }
+    keyUpFunc(event:any){//キーボードが押されたときに呼び出される関数
+        let key_code = event.keyCode
+        switch(key_code){
+            case 65://「A」キーが離されたとき
+            case 37://「←」キーが離されたとき
+                this.key_left=false
+            case 68://「D」キーが離されたとき
+            case 39://「→」キーが離されたとき
+                this.key_right=false
+            case 32://「Space」キーが離されたとき
+            case 96://「0(テンキー)」キーが離されたとき
+                this.key_jump=false
+            }
     }
 }
 
 let rabbit=new character()
+let key=new keyDown()
+requestAnimationFrame(main)//メインループ、起動
 
- document.getElementById('character')!.onclick=function(){
-    rabbit.y += 32
-    document.getElementById('character')!.style.top=rabbit.y+"px"
-} 
+function main(){//メインループ
+    addEventListener("keydown",key.keyDownFunc)//キーボードが押された時、keyDownFunc関数を呼び出す
+    addEventListener("keyup",key.keyUpFunc)//キーボードが離された時、keyUpFunc関数を呼び出す
 
-//document.getElementById('character')!.onclick=rabbit.move()
+/*     【仕様】
+    左右キーは同時に押すとどちらにも移動できない(どちらか片方を押しているときのみ移動できる)
+    ジャンプはジャンプキーを押している間に跳躍力を貯めて、ジャンプキーを離すと貯めた跳躍力の分だけ跳べる
+    跳躍力を貯めている間は接地中の移動ができない */
+
+	if(((key.key_left===true)&&(key.key_right===false))&&(!((key.key_jump==true)&&(rabbit.isOnGround==true)))){//左移動キーが押されている間、moveLeft関数を呼び出す
+        rabbit.moveLeft()
+    }
+	if(((key.key_right===true)&&(key.key_left===false))&&(!((key.key_jump==true)&&(rabbit.isOnGround==true)))){//右移動キーが押されている間、moveRight関数を呼び出す
+        rabbit.moveRight()
+    }
+    if(key.key_right===true){
+        rabbit.x+=20
+        document.getElementById('character')!.style.left =rabbit.x+"px"
+    }
+    if((key.key_jump===true)){//ジャンプキーが押されている間、jumpCharge関数を呼び出す
+        rabbit.jumpCharge()
+    }
+    rabbit.move()
+    requestAnimationFrame(main)////main関数(自分自身)を呼び出すことでループさせる
+}
