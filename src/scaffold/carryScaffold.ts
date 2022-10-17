ee308a6d471842ad253d3039f9c2e0a9711ac410
†村神† <107534447+CA01971172@users.noreply.github.com>
2022-10-17 19:47:29 +0900

    Make this game difficult

--- src/scaffold/carryScaffold.ts
+++ src/scaffold/carryScaffold.ts
@@ -2,7 +2,7 @@ import {scaffold} from "./scaffold"
 
 export class carryScaffold extends scaffold{//キャラが乗ると動かされる足場
     private _direction:number=1//キャラが動かされる方向 左が0,右が1
-    public readonly carryVelocity=2//キャラを運ぶスピード(絶対値)
+    public readonly carryVelocity=3//キャラを運ぶスピード(絶対値)
     constructor(_level:number,_width:number=scaffold.defaultWidth){
         super(_level,_width)
         this.direction=Math.floor(Math.random()*2)//向きを0(左)か1(右)でランダムに代入
