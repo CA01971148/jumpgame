df0433f155d8a178f20b98ad09d15fa0ec009268
†村神† <CA01971172@st.kawahara.ac.jp>
2022-11-02 12:11:00 +0900

    use the setter of friction in scaffold class

--- src/scaffold/slipScaffold.ts
+++ src/scaffold/slipScaffold.ts
@@ -6,7 +6,7 @@ import {testList} from "./../other/test"
 export class slipScaffold extends scaffold{
     constructor(_level:number,_width:number=scaffold.defaultWidth){
         super(_level,_width)
-        this._friction=0.01
+        this.friction=0.01
         this.imageName="slipScaffold.jpg"
         this.createImgElement(this.imageName)//足場のimg要素を追加する
         this.setImgElement()//img要素を取得して初期化する
